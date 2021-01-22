27cd2bb49f9c484aab080db8a745bfa9317842bb
dilini1995s <dilinitharanga721@gmail.com>
2021-01-22 12:46:40 +0530

    refactor the code

--- ui/web/frontend/src/App.js
+++ ui/web/frontend/src/App.js
@@ -20,12 +20,12 @@ class App extends Component{
       const dat1 = this.state.username;
       const dat2 = this.state.password;
       Axios({
-              method: 'POST',
-              url: 'http://localhost:80/Practical_test/index.php',
-              headers: {
+            method: 'POST',
+            url: 'http://localhost:80/Practical_test/index.php',
+            headers: {
                 'Content-Type': 'application/json',
               },
-              data: {
+            data: {
                 dat1,
                 dat2
               }
@@ -35,7 +35,7 @@ class App extends Component{
             })
             .catch((error) => {
               console.log(error)
-            })
+        })
 
   }
 
@@ -46,6 +46,7 @@ class App extends Component{
             <h3>Login form</h3>
           </nav>
           <br></br>
+
           <form method="POST" onSubmit={this.submitHandler}>
               <div className="form-group">
                   <input type="text" name="username" class="form-control" placeholder="username" onChange={(event)=>{
@@ -54,7 +55,9 @@ class App extends Component{
                       });
                   }} value={this.state.username} />
               </div>
+
                   <br></br><br></br>
+
               <div className="form-group">
                   <input type="text" name="password" class="form-control" placeholder="password" onChange={(event)=>{
                       this.setState({
@@ -63,7 +66,7 @@ class App extends Component{
                   }} value={this.state.password} />
               </div><br></br><br></br>
 
-                  <input type="submit" value="Login" className="btn btn-dark btn-lg btn-block" name="submit" />
+                <input type="submit" value="Login" className="btn btn-dark btn-lg btn-block" name="submit" />
             </form>
               <a href="/user" className="navbar-brand">
                 UserList
@@ -74,9 +77,8 @@ class App extends Component{
           <Switch>
             <Route exact path="/user" component={userList} />
           </Switch>
-          </BrowserRouter>
+        </BrowserRouter>
         </div>
-
         </div>
       );
   }
